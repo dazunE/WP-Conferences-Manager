99411cc9cd58bf1728e47c204b46d8c5cea51ad1
Dasun Edirisinghe <dazunj4me@gmail.com>
2019-02-27 23:46:47 +0530

    front-end of conference single

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -3,9 +3,15 @@
 namespace Conferences;
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\boot_carbon_fields' );
+add_action( 'wp_enqueue_scripts' , __NAMESPACE__ . '\\enques_front_end_styles' );
 
 
 function boot_carbon_fields() {
 	\Carbon_Fields\Carbon_Fields::boot();
 }
 
+function enques_front_end_styles () {
+	if( is_singular('ccm_conferences')){
+		wp_enqueue_style( 'single-conferences', CEYLON_CONF_PLUGIN_URL.'/assets/css/conferences.css');
+	}
+}
--- includes/helpers/helpers.php
+++ includes/helpers/helpers.php
@@ -22,7 +22,7 @@ function session_list_display( $id, $meta_field ) {
 
 			$first_session = $session['ccm_session_post_type'][0]['id'];
 
-			$date = 'Day 0'.$day.' - '.get_session_date_by_id($first_session);
+			$date = 'Day 0' . $day . ' - ' . get_session_date_by_id( $first_session );
 
 			echo sprintf(
 				'<div class="sessions-wrap">
@@ -30,7 +30,7 @@ function session_list_display( $id, $meta_field ) {
 			    		<div class="sessions-row columns-%s">',
 				$date,
 				$rows
-				);
+			);
 
 			foreach ( $session['ccm_session_post_type'] as $item ) {
 
@@ -38,7 +38,7 @@ function session_list_display( $id, $meta_field ) {
 
 			}
 
-			echo sprintf('</div></div>');
+			echo sprintf( '</div></div>' );
 
 		}
 	}
@@ -47,25 +47,24 @@ function session_list_display( $id, $meta_field ) {
 
 function get_session_data_by_id( $session_id ) {
 
-	$post = get_post($session_id );
-
+	$post = get_post( $session_id );
 
 	?>
 
-	<div class="single-session">
-		<h4><?php echo $post->post_title; ?></h4>
-		<p class="session-time">
+    <div class="single-session">
+        <h4><?php echo $post->post_title; ?></h4>
+        <p class="session-time">
 			<span class="start-time">
-				<?php echo carbon_get_post_meta( $session_id , 'ccm_start_time')?>
+				<?php echo carbon_get_post_meta( $session_id, 'ccm_start_time' ) ?>
 			</span>
-			<span class="end-time">
-				<?php echo carbon_get_post_meta( $session_id , 'ccm_end_time')?>
+            <span class="end-time">
+				<?php echo carbon_get_post_meta( $session_id, 'ccm_end_time' ) ?>
 			</span>
-		</p>
-		<?php echo $post->post_content;?>
-	</div>
+        </p>
+		<?php echo $post->post_content; ?>
+    </div>
 
-    <?php
+	<?php
 
 
 }
@@ -73,4 +72,37 @@ function get_session_data_by_id( $session_id ) {
 function get_session_date_by_id( $session_id ) {
 
 	return carbon_get_post_meta( $session_id, 'ccm_session_date' );
+}
+
+function get_conference_header_section( $conference_id ){
+
+    $page_title = carbon_get_post_meta( $conference_id , 'ccm_page_title' );
+    $page_teaser = carbon_get_post_meta( $conference_id ,'ccm_conference_teaser');
+	$conference_header = carbon_get_post_meta($conference_id , 'ccm_conference_header');
+	$conference_intro = carbon_get_post_meta( $conference_id , 'ccm_conference_paragraph' );
+	$confernce_video = carbon_get_post_meta( $conference_id ,'ccm_conference_video');
+
+    if( !$page_title ){
+        echo sprintf('<h2>%s</h2>', get_the_title( $conference_id ) );
+    }
+
+    if( !empty( $page_teaser ) ){
+	    echo sprintf('<h2>%s</h2>', $page_teaser );
+    }
+
+	if( !empty( $conference_header ) ){
+		echo sprintf('<h3>%s</h3>', $conference_header );
+	}
+
+	if( !empty( $conference_intro ) ) {
+	    echo sprintf( '<div class="conference-intro">%s</div>' , wpautop($conference_intro) );
+    }
+
+	if( !empty( $confernce_video) ){
+	    echo sprintf( '<div class="conference-video">%s</div>', wp_oembed_get( $confernce_video )
+            );
+    }
+
+
+
 }
