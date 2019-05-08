36b97dc282ab166248e5c10460fcaa82ed8d28bb
Dasun Edirisinghe <dazunj4me@gmail.com>
2019-05-08 15:14:44 +0530

    search shortcode

--- includes/shortcode/shortcodes.php
+++ includes/shortcode/shortcodes.php
@@ -10,7 +10,7 @@ function speaker_short_code( $atts ) {
 			'session'    => null,
 			'conference' => null,
 			'bio'        => true,
-			'social'    => true
+			'social'     => true
 		),
 		$atts,
 		'ccm_speakers'
@@ -26,8 +26,8 @@ function speaker_short_code( $atts ) {
 
 		foreach ( array_unique( $speaker_ids ) as $speaker_id ) {
 
-			echo'<div class="speaker-profile">';
-			Helpers\get_the_speaker_profile_by_id( $speaker_id , $atts['bio'] , $atts['social']);
+			echo '<div class="speaker-profile">';
+			Helpers\get_the_speaker_profile_by_id( $speaker_id, $atts['bio'], $atts['social'] );
 			echo '</div>';
 
 		}
@@ -42,4 +42,24 @@ function speaker_short_code( $atts ) {
 
 add_shortcode( 'ccm_speakers', 'speaker_short_code' );
 
+function speaker_session_combine_search() {
 
+	$args_speaker = get_posts(
+		array(
+			'post_type' => SPEAKER_POST_TYPE,
+			'post_status' => 'publish',
+			's' => 'Edirisinghe'
+		)
+	);
+
+	$args_sessions = get_posts(
+
+	);
+
+	var_dump( $args_speaker );
+
+
+
+}
+
+add_shortcode( 'ccm_search', 'speaker_session_combine_search' );
